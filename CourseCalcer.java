e5b7eac8a9526ba2ae6a29e607949c15bc85d0fe
Abhay <114037744+SenatorDingo@users.noreply.github.com>
2023-05-08 08:49:50 -0400

    Starting Of TermList Implementation
    Added a new termlist implementation to hold term data

--- CourseCalcer.java
+++ CourseCalcer.java
@@ -1,8 +1,8 @@
 
 import java.util.Scanner;
 
-public class CourseCalcer{
-	
+public class CourseCalcer {
+	 
 	
 	public static void main(String[] args){
 		
---
+++ TermList.java
@@ -0,0 +1,13 @@
+protected class TermList extends LinkedList{
+	
+	protected termList;
+	
+	
+	
+	public TermList(){
+		
+		termList = new LinkedList();
+		
+		
+		
+		
